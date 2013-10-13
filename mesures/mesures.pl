a75c8a387df15b878a572d5cf4b7d9695fb029a6
Grégory Mounié <Gregory.Mounie@imag.fr>
2013-10-13 12:27:14 +0200

    script pour les mesures

---
+++ mesures/mesures.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl -w
+
+use warnings;
+use strict;
+use autodie;
+use Sys::Hostname;
+
+my $fname= "mesures.csv";
+my $tsp="../build/ensitsp";
+
+my @allnthreads= (1, 2, 4, 8, 16, 32, 64);
+my @allncities= (15..18);
+my @allseeds= (12388);
+
+if ( ! -e $fname ) {
+    open my $fh, ">", $fname;
+    print $fh "date; machine; nvilles; graine; nthreads; longueur; temps; coupures\n";
+    close $fh;
+}
+
+my $machine= hostname();
+
+open my $fh, ">>", $fname;
+
+for my $nthreads ( @allnthreads ) {
+    for my $seed ( @allseeds ) {
+	for my $ncities ( @allncities ) {
+	    my $command="${tsp} ${ncities} ${seed} ${nthreads}";
+	    print $command."\n";
+	    open my $expe, "-|", $command;
+	    my $date = localtime();
+	    while ( my $line= <$expe> ) {
+		chomp($line);
+		my @val = split(" ",$line);
+		print $fh join(";",$date,$machine,
+				  $val[3],$val[6],$val[12],
+				  $val[9],$val[15], $val[18])."\n"; 
+	    }
+	    close($expe)
+	}
+    }
+}
+
+close $fh;
