25d77b4ca6f25620d7ec906d424d050d734a0b63
Grégory Mounié <Gregory.Mounie@imag.fr>
2013-10-13 15:20:46 +0200

    script pour les figures

--- mesures/mesures.pl
+++ mesures/mesures.pl
@@ -10,7 +10,7 @@ my $tsp="../build/ensitsp";
 
 my @allnthreads= (1, 2, 4, 8, 16, 32, 64);
 my @allncities= (15..18);
-my @allseeds= (12388);
+my @allseeds= (12387, 12388);
 
 if ( ! -e $fname ) {
     open my $fh, ">", $fname;
@@ -42,3 +42,5 @@ for my $nthreads ( @allnthreads ) {
 }
 
 close $fh;
+
+# Genérer les figures
